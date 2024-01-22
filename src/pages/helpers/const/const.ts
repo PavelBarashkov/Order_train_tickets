40b8d9ec016bd9b86ff8a5f15803322513ac653e
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2024-01-22 16:59:07 +0400

    add router Main page

---
+++ src/pages/helpers/const/const.ts
@@ -0,0 +1,4 @@
+export const MAIN_ROUTE = "/"
+export const CHOICE_TRAIN_ROUTE = "/choice/train"
+export const CHOICE_TICKET_ROUTE = "/choice/ticket"
+export const PAYMENT_TICKET_ROUTE = '/payment/ticket'
