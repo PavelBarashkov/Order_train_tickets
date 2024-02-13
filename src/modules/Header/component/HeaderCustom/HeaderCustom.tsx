ed16843c31c683cba29bfd304f462088e7190e47
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2024-02-13 22:06:17 +0400

    init

--- src/modules/Header/component/HeaderCustom/HeaderCustom.tsx
+++ src/modules/Header/component/HeaderCustom/HeaderCustom.tsx
@@ -1,10 +1,19 @@
 import type React from "react"
 import classes from "./headerCustom.module.css"
+import { useLocation } from "react-router-dom"
+import { MAIN_ROUTE } from "../../../../pages/helpers/const/const"
 
 interface HeaderCustomProps {
   children: React.ReactNode
 }
 
 export const HeaderCustom = ({ children }: HeaderCustomProps) => {
-  return <header className={classes.header}> {children} </header>
+  const location = useLocation()
+  const isMain = location.pathname === MAIN_ROUTE
+
+  if (isMain) {
+    return <header className={classes.header}> {children} </header>
+  }
+
+  return <header className={classes.headerSub}> {children} </header>
 }
--- src/modules/Header/component/MainInfo/MainInfo.tsx
+++ src/modules/Header/component/MainInfo/MainInfo.tsx
@@ -1,9 +1,15 @@
 import { Container } from "react-bootstrap"
 import { Title } from "../Title/Title"
 import classes from "./mainInfo.module.css"
+import { useLocation } from "react-router-dom"
+import { MAIN_ROUTE } from "../../../../pages/helpers/const/const"
 
 export const MainInfo = () => {
-  return (
+  const location = useLocation();
+  const isMain = location.pathname === MAIN_ROUTE;
+
+  if (isMain) {
+    return (
     <div className={classes.MainInfo}>
       <Container style={{ maxWidth: 1480 }}>
         <div className={classes.containerInfo}>
@@ -12,5 +18,16 @@ export const MainInfo = () => {
         </div>
       </Container>
     </div>
+    )
+  }
+
+  return (
+    <div className={classes.MainInfo}>
+    <Container>
+      <div className={classes.containerInfo}>
+        <form className={classes.form} action=""></form>
+      </div>
+    </Container>
+  </div>
   )
 }
