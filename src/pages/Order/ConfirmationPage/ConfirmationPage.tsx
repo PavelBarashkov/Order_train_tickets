fab78f8cba649749d418f15103c5e399e060561f
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2024-03-11 20:04:27 +0400

    add Confirmation component

--- src/pages/Order/ConfirmationPage/ConfirmationPage.tsx
+++ src/pages/Order/ConfirmationPage/ConfirmationPage.tsx
@@ -1,7 +1,21 @@
 import type React from 'react'
+import { Header, PageContainerWithSidBar } from '../../../layouts'
+import { Logo, Navigation } from '../../../components'
+import { FormSearchTickets, PassengerRegistration } from '../../../modules'
+import { Confirmation } from '../../../components/Confirmation/Confirmation'
 
 export const ConfirmationPage: React.FC = () => {
   return (
-    <div>ConfirmationPage</div>
+    <>
+    <Header
+      logo={<Logo logo="Лого" />}
+      navBar={<Navigation.NavBar />}
+      form={<FormSearchTickets.FormSearchTickets isMain={false} />}
+      stageBar={<Navigation.StageBar />}
+    />
+    <PageContainerWithSidBar asideBar={<PassengerRegistration.AsideBar />}>
+      <Confirmation/>
+    </PageContainerWithSidBar>
+  </>
   )
 }
